tsubasa/OpenTween

commit 12b51b8caff9f1873a00966b692d77531d97d108

TwitterApi.StatusesUpdateメソッドに新たに指定可能となったパラメータを追加
https://dev.twitter.com/overview/api/upcoming-changes-to-tweets に記載
されている下記のパラメータを引数に追加:

 * auto_populate_reply_metadata
 * exclude_reply_user_ids

OpenTween/Api/TwitterApi.cs
@@ -126,7 +126,8 @@ namespace OpenTween.Api
             return this.apiConnection.GetAsync<TwitterStatus>(endpoint, param, "/statuses/show/:id");
         }
 
-        public Task<LazyJson<TwitterStatus>> StatusesUpdate(string status, long? replyToId, IReadOnlyList<long> mediaIds)
+        public Task<LazyJson<TwitterStatus>> StatusesUpdate(string status, long? replyToId, IReadOnlyList<long> mediaIds,
+            bool? autoPopulateReplyMetadata = null, IReadOnlyList<long> excludeReplyUserIds = null)
         {
             var endpoint = new Uri("statuses/update.json", UriKind.Relative);
             var param = new Dictionary<string, string>
@@ -141,6 +142,10 @@ namespace OpenTween.Api
                 param["in_reply_to_status_id"] = replyToId.ToString();
             if (mediaIds != null && mediaIds.Count > 0)
                 param.Add("media_ids", string.Join(",", mediaIds));
+            if (autoPopulateReplyMetadata != null)
+                param["auto_populate_reply_metadata"] = autoPopulateReplyMetadata.Value ? "true" : "false";
+            if (excludeReplyUserIds != null)
+                param["exclude_reply_user_ids"] = string.Join(",", excludeReplyUserIds);
 
             return this.apiConnection.PostLazyAsync<TwitterStatus>(endpoint, param);
         }

OpenTween.Tests/Api/TwitterApiTest.cs
@@ -221,13 +221,16 @@ namespace OpenTween.Api
                             { "tweet_mode", "extended" },
                             { "in_reply_to_status_id", "100" },
                             { "media_ids", "10,20" },
+                            { "auto_populate_reply_metadata", "true" },
+                            { "exclude_reply_user_ids", "100,200" },
                         })
                 )
                 .ReturnsAsync(LazyJson.Create(new TwitterStatus()));
 
                 twitterApi.apiConnection = mock.Object;
 
-                await twitterApi.StatusesUpdate("hogehoge", replyToId: 100L, mediaIds: new[] { 10L, 20L })
+                await twitterApi.StatusesUpdate("hogehoge", replyToId: 100L, mediaIds: new[] { 10L, 20L },
+                        autoPopulateReplyMetadata: true, excludeReplyUserIds: new[] { 100L, 200L })
                     .IgnoreResponse()
                     .ConfigureAwait(false);
 

commit 5e80180fba49b1d778b8ab5078cc0ada4d0b6792

exclude_reply_user_ids が空の場合にはパラメータ自体を送信しない
Fixes: 12b51b8c ("TwitterApi.StatusesUpdateメソッドに新たに指定可能となったパラメータを追加")

OpenTween/Api/TwitterApi.cs
@@ -144,7 +144,7 @@ namespace OpenTween.Api
                 param.Add("media_ids", string.Join(",", mediaIds));
             if (autoPopulateReplyMetadata != null)
                 param["auto_populate_reply_metadata"] = autoPopulateReplyMetadata.Value ? "true" : "false";
-            if (excludeReplyUserIds != null)
+            if (excludeReplyUserIds != null && excludeReplyUserIds.Count > 0)
                 param["exclude_reply_user_ids"] = string.Join(",", excludeReplyUserIds);
 
             return this.apiConnection.PostLazyAsync<TwitterStatus>(endpoint, param);

OpenTween.Tests/Api/TwitterApiTest.cs
@@ -239,6 +239,35 @@ namespace OpenTween.Api
         }
 
         [Fact]
+        public async Task StatusesUpdate_ExcludeReplyUserIdsEmptyTest()
+        {
+            using (var twitterApi = new TwitterApi())
+            {
+                var mock = new Mock<IApiConnection>();
+                mock.Setup(x =>
+                    x.PostLazyAsync<TwitterStatus>(
+                        new Uri("statuses/update.json", UriKind.Relative),
+                        new Dictionary<string, string> {
+                            { "status", "hogehoge" },
+                            { "include_entities", "true" },
+                            { "include_ext_alt_text", "true" },
+                            { "tweet_mode", "extended" },
+                            // exclude_reply_user_ids は空の場合には送信されない
+                        })
+                )
+                .ReturnsAsync(LazyJson.Create(new TwitterStatus()));
+
+                twitterApi.apiConnection = mock.Object;
+
+                await twitterApi.StatusesUpdate("hogehoge", replyToId: null, mediaIds: null, excludeReplyUserIds: new long[0])
+                    .IgnoreResponse()
+                    .ConfigureAwait(false);
+
+                mock.VerifyAll();
+            }
+        }
+
+        [Fact]
         public async Task StatusesDestroy_Test()
         {
             using (var twitterApi = new TwitterApi())