tsubasa/OpenTween

commit bcb24ce76f9ea5d186e007d69677fa3835db8b6c

attachment_urlに指定可能なURLを140字のカウントから除外する

OpenTween/Twitter.cs
@@ -131,6 +131,15 @@ namespace OpenTween
         public static readonly Regex StatusUrlRegex = new Regex(@"https?://([^.]+\.)?twitter\.com/(#!/)?(?<ScreenName>[a-zA-Z0-9_]+)/status(es)?/(?<StatusId>[0-9]+)(/photo)?", RegexOptions.IgnoreCase);
 
         /// <summary>
+        /// attachment_url に指定可能な URL を判定する正規表現
+        /// </summary>
+        public static readonly Regex AttachmentUrlRegex = new Regex(@"https?://(
+   twitter\.com/[0-9A-Za-z]+/status/[0-9]+
+ | mobile\.twitter\.com/[0-9A-Za-z]+/status/[0-9]+
+ | twitter\.com/messages/compose\?recipient_id=[0-9]+(&.+)?
+)$", RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace);
+
+        /// <summary>
         /// FavstarやaclogなどTwitter関連サービスのパーマリンクURLからステータスIDを抽出する正規表現
         /// </summary>
         public static readonly Regex ThirdPartyStatusUrlRegex = new Regex(@"https?://(?:[^.]+\.)?(?:

OpenTween.Tests/TwitterTest.cs
@@ -54,6 +54,22 @@ namespace OpenTween
         }
 
         [Theory]
+        [InlineData("https://twitter.com/twitterapi/status/22634515958", true)]
+        [InlineData("http://twitter.com/twitterapi/status/22634515958", true)]
+        [InlineData("https://mobile.twitter.com/twitterapi/status/22634515958", true)]
+        [InlineData("http://mobile.twitter.com/twitterapi/status/22634515958", true)]
+        [InlineData("https://twitter.com/i/web/status/22634515958", false)]
+        [InlineData("https://twitter.com/imgazyobuzi/status/293333871171354624/photo/1", false)]
+        [InlineData("https://pic.twitter.com/gbxdb2Oj", false)]
+        [InlineData("https://twitter.com/messages/compose?recipient_id=514241801", true)]
+        [InlineData("http://twitter.com/messages/compose?recipient_id=514241801", true)]
+        [InlineData("https://twitter.com/messages/compose?recipient_id=514241801&text=%E3%81%BB%E3%81%92", true)]
+        public void AttachmentUrlRegexTest(string url, bool isMatch)
+        {
+            Assert.Equal(isMatch, Twitter.AttachmentUrlRegex.IsMatch(url));
+        }
+
+        [Theory]
         [InlineData("http://favstar.fm/users/twitterapi/status/22634515958", new[] { "22634515958" })]
         [InlineData("http://ja.favstar.fm/users/twitterapi/status/22634515958", new[] { "22634515958" })]
         [InlineData("http://favstar.fm/t/22634515958", new[] { "22634515958" })]

OpenTween/Tween.cs
@@ -2130,8 +2130,11 @@ namespace OpenTween
             CheckReplyTo(StatusText.Text);
 
             long[] autoPopulatedUserIds;
-
             var statusText = this.RemoveAutoPopuratedMentions(this.StatusText.Text, out autoPopulatedUserIds);
+
+            string attachmentUrl;
+            statusText = this.RemoveAttachmentUrl(statusText, out attachmentUrl);
+
             statusText = this.FormatStatusText(statusText);
 
             if (this.GetRestStatusCount(statusText) < 0)
@@ -2155,6 +2158,8 @@ namespace OpenTween
                 status.excludeReplyUserIds = replyToPost.ReplyToList.Select(x => x.Item1).Except(autoPopulatedUserIds)
                     .ToArray();
             }
+
+            status.attachmentUrl = attachmentUrl;
 
             if (ImageSelector.Visible)
             {
@@ -4770,6 +4775,27 @@ namespace OpenTween
         }
 
         /// <summary>
+        /// attachment_url に指定可能な URL が含まれていれば除去
+        /// </summary>
+        private string RemoveAttachmentUrl(string statusText, out string attachmentUrl)
+        {
+            var match = Twitter.AttachmentUrlRegex.Match(statusText);
+            if (!match.Success)
+            {
+                attachmentUrl = null;
+                return statusText;
+            }
+
+            attachmentUrl = match.Value;
+
+            // マッチした URL を空白に置換
+            statusText = statusText.Substring(0, match.Index);
+
+            // テキストと URL の間にスペースが含まれていれば除去
+            return statusText.TrimEnd(' ');
+        }
+
+        /// <summary>
         /// ツイート投稿前のフッター付与などの前処理を行います
         /// </summary>
         private string FormatStatusText(string statusText)
@@ -4859,6 +4885,9 @@ namespace OpenTween
         {
             long[] autoPopulatedUserIds;
             statusText = this.RemoveAutoPopuratedMentions(statusText, out autoPopulatedUserIds);
+
+            string attachmentUrl;
+            statusText = this.RemoveAttachmentUrl(statusText, out attachmentUrl);
 
             statusText = this.FormatStatusText(statusText);
 

commit fcc5dda2b128fec1f5a2207ed11ecd866cc47744

アンダーバーを含むscreen_nameがAttachmentUrlRegexで考慮されていない不具合を修正
Fixes: bcb24ce7 ("attachment_urlに指定可能なURLを140字のカウントから除外する")

OpenTween/Twitter.cs
@@ -134,8 +134,8 @@ namespace OpenTween
         /// attachment_url に指定可能な URL を判定する正規表現
         /// </summary>
         public static readonly Regex AttachmentUrlRegex = new Regex(@"https?://(
-   twitter\.com/[0-9A-Za-z]+/status/[0-9]+
- | mobile\.twitter\.com/[0-9A-Za-z]+/status/[0-9]+
+   twitter\.com/[0-9A-Za-z_]+/status/[0-9]+
+ | mobile\.twitter\.com/[0-9A-Za-z_]+/status/[0-9]+
  | twitter\.com/messages/compose\?recipient_id=[0-9]+(&.+)?
 )$", RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace);
 