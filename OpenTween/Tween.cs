tsubasa/OpenTween

commit b8bb46c16688896c4999cbae844af9c22995fc1d

auto_populate_reply_metadataによって自動で付加されるメンションを抽出し、送信するtextパラメータから除去する
auto_populate_reply_metadata を true にした場合、下記のユーザーに対して自動で
先頭にメンションが付加される。

 * in_reply_to_status_id の宛先のツイートを投稿したユーザー
 * in_reply_to_status_id の宛先のツイートに含まれる全てのメンション先ユーザー

これらのユーザーに対するメンションは、text パラメータ内にメンションが既に
含まれていても重複して付与されてしまうため、事前に除去する必要がある。

一方で、投稿欄に入力されているメンションのみを忠実に付与させるため、ユーザーが
入力していないメンションが auto_populate_reply_metadata によって付与されて
しまう場合は exclude_reply_user_ids パラメータを使用して明示的に除外する。

OpenTween/Tween.cs
@@ -997,7 +997,7 @@ namespace OpenTween
             StatusLabel.AutoToolTip = false;
             StatusLabel.ToolTipText = "";
             //文字カウンタ初期化
-            lblLen.Text = this.GetRestStatusCount(this.FormatStatusText("")).ToString();
+            lblLen.Text = this.GetRestStatusCount("").ToString();
 
             this.JumpReadOpMenuItem.ShortcutKeyDisplayString = "Space";
             CopySTOTMenuItem.ShortcutKeyDisplayString = "Ctrl+C";
@@ -2128,7 +2128,10 @@ namespace OpenTween
             StatusText.SelectionStart = StatusText.Text.Length;
             CheckReplyTo(StatusText.Text);
 
-            var statusText = this.FormatStatusText(this.StatusText.Text);
+            long[] autoPopulatedUserIds;
+
+            var statusText = this.RemoveAutoPopuratedMentions(this.StatusText.Text, out autoPopulatedUserIds);
+            statusText = this.FormatStatusText(statusText);
 
             if (this.GetRestStatusCount(statusText) < 0)
             {
@@ -2143,6 +2146,15 @@ namespace OpenTween
 
             status.inReplyToId = this.inReplyTo?.Item1;
             status.inReplyToName = this.inReplyTo?.Item2;
+
+            var replyToPost = this.inReplyTo != null ? this._statuses[this.inReplyTo.Item1] : null;
+            if (replyToPost != null)
+            {
+                // ReplyToList のうち autoPopulatedUserIds に含まれていないユーザー ID を抽出
+                status.excludeReplyUserIds = replyToPost.ReplyToList.Select(x => x.Item1).Except(autoPopulatedUserIds)
+                    .ToArray();
+            }
+
             if (ImageSelector.Visible)
             {
                 //画像投稿
@@ -4707,7 +4719,7 @@ namespace OpenTween
         private void StatusText_TextChanged(object sender, EventArgs e)
         {
             //文字数カウント
-            int pLen = this.GetRestStatusCount(this.FormatStatusText(this.StatusText.Text));
+            int pLen = this.GetRestStatusCount(this.StatusText.Text);
             lblLen.Text = pLen.ToString();
             if (pLen < 0)
             {
@@ -4724,6 +4736,36 @@ namespace OpenTween
             {
                 this.inReplyTo = null;
             }
+        }
+
+        /// <summary>
+        /// 投稿時に auto_populate_reply_metadata オプションによって自動で追加されるメンションを除去します
+        /// </summary>
+        private string RemoveAutoPopuratedMentions(string statusText, out long[] autoPopulatedUserIds)
+        {
+            List<long> _autoPopulatedUserIds = new List<long>();
+
+            var replyToPost = this.inReplyTo != null ? this._statuses[this.inReplyTo.Item1] : null;
+            if (replyToPost != null)
+            {
+                if (statusText.StartsWith($"@{replyToPost.ScreenName} ", StringComparison.Ordinal))
+                {
+                    statusText = statusText.Substring(replyToPost.ScreenName.Length + 2);
+
+                    foreach (var reply in replyToPost.ReplyToList)
+                    {
+                        if (statusText.StartsWith($"@{reply.Item2} ", StringComparison.Ordinal))
+                        {
+                            statusText = statusText.Substring(reply.Item2.Length + 2);
+                            _autoPopulatedUserIds.Add(reply.Item1);
+                        }
+                    }
+                }
+            }
+
+            autoPopulatedUserIds = _autoPopulatedUserIds.ToArray();
+
+            return statusText;
         }
 
         /// <summary>
@@ -4814,7 +4856,11 @@ namespace OpenTween
         /// </summary>
         private int GetRestStatusCount(string statusText)
         {
-            //文字数カウント
+            long[] autoPopulatedUserIds;
+            statusText = this.RemoveAutoPopuratedMentions(statusText, out autoPopulatedUserIds);
+
+            statusText = this.FormatStatusText(statusText);
+
             var remainCount = this.tw.GetTextLengthRemain(statusText);
 
             var uploadService = this.ImageSelector.SelectedService;

commit 5badd4c70d627355983da95e647e6999acbaf6ce

RemoveAutoPopuratedMentionsが返すユーザーIDにリプライ先のユーザーも含める
Fixes: b8bb46c1 ("auto_populate_reply_metadataによって自動で付加されるメンションを抽出し、送信するtextパラメータから除去する")

OpenTween/Tween.cs
@@ -4760,6 +4760,7 @@ namespace OpenTween
                 if (statusText.StartsWith($"@{replyToPost.ScreenName} ", StringComparison.Ordinal))
                 {
                     statusText = statusText.Substring(replyToPost.ScreenName.Length + 2);
+                    _autoPopulatedUserIds.Add(replyToPost.UserId);
 
                     foreach (var reply in replyToPost.ReplyToList)
                     {